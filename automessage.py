a8c5b731755003ef309fff7adca9c8b1a6c6ddc9
PowaBanga <PowaBanga@users.noreply.github.com>
2017-08-04 18:32:04 +0200

    add automessage

---
+++ automessage.py
@@ -0,0 +1,23 @@
+__module_name__ = "Auto messages"
+__module_version__ = "0.1"
+__module_description__ = "send messages when some word appear in dialog"
+__module_author__ = "Powabanga"
+
+import xchat
+
+    if word[1]=="!bug":
+        xchat.command("say  14Message1 :14")
+        xchat.command("say 00Message2 :")
+
+    if word[1]=="!blabla":
+        xchat.command("say  14Message3 :14 ")
+        xchat.command("say 00Message4")
+
+
+    return xchat.EAT_NONE
+
+
+# lance les fonctions
+EVENTS = [("Channel Message", 1)]
+for event in EVENTS:
+    xchat.hook_print(event[0], stop_mess_auto, event)
